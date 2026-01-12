50e3a87e4df49ec828d8292ef58536bfd23e999d
asebrech <alois.sebrechts@gmail.com>
2026-01-12 13:37:42 +0100

    looknfeel

--- home/common/optional/desktops/hyprland/default.nix
+++ home/common/optional/desktops/hyprland/default.nix
@@ -1,7 +1,6 @@
 {
   osConfig,
   pkgs,
-  lib,
   ...
 }:
 
@@ -13,6 +12,7 @@
     ./input.nix
     ./autostart.nix
     ./windows.nix
+    ./looknfeel.nix
 
     #hyprland utilities
     ./hyprlock.nix
@@ -28,10 +28,6 @@
       variables = [ "--all" ]; # fix for https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/#programs-dont-work-in-systemd-services-but-do-on-the-terminal
     };
     settings = {
-      debug = {
-        disable_logs = true;
-      };
-      # Environment variables and ecosystem settings are now configured in ./envs.nix
       #
       # ========== Monitor ==========
       #
@@ -50,88 +46,7 @@
           }"
         ) osConfig.monitors
       );
-
-      workspace = (
-        let
-          workspaceIDs = lib.flatten [
-            (lib.range 1 10) # workspaces 1 through 10, Hyprland does not allow ws 0 :/
-            "special" # add the special/scratchpad ws
-          ];
-          isPrimary = x: x ? primary && x.primary;
-          primary = lib.lists.findFirst isPrimary { } osConfig.monitors;
-        in
-        # workspace structure to build "[workspace], monitor:[name], default:[bool], persistent:[bool]"
-        map (
-          id:
-          let
-            id_as_string = toString id;
-            # determine if the monitor is intended to display a specific workspace
-            monitor = lib.lists.findFirst (
-              x: x ? "workspace" && id_as_string == x.workspace
-            ) primary osConfig.monitors;
-            # workspace 1 and any workspaces specific to the non-primary monitors are persistent
-            persistent = if (id == 1 || !(isPrimary monitor)) then ", persistent:true" else "";
-          in
-          "${id_as_string}, monitor:${monitor.name}, default:true" + persistent
-        ) workspaceIDs
-      );
-      #
-      # ========== Behavior ==========
-      #
-      binds = {
-        workspace_center_on = 1; # Whether switching workspaces should center the cursor on the workspace (0) or on the last active window for that workspace (1)
-      };
-      cursor = {
-        inactive_timeout = 3;
-        hide_on_key_press = true;
-        enable_hyprcursor = true;
-      };
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        resize_on_border = true;
-        allow_tearing = false;
-      };
-      decoration = {
-        rounding = 10;
-        active_opacity = 0.95;
-        inactive_opacity = 0.85;
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-          vibrancy = 0.1696;
-        };
-      };
-      animations = {
-        enabled = true;
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
-      };
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-      master = {
-        new_status = "master";
-      };
-      # Input and misc settings are now configured in ./input.nix
-      gestures = {
-        workspace_swipe = false;
-      };
-
-      # Autostart applications are now configured in ./autostart.nix
-
     };
-
   };
 
   programs.zsh.shellAliases = {
