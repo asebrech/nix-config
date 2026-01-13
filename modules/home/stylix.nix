ad7ff08754543aef756bcbe03f53ccd2305f47a8
asebrech <alois.sebrechts@gmail.com>
2026-01-13 11:33:06 +0100

    fix stylix

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -11,30 +11,6 @@
         # Terminal emulators
         alacritty.enable = true;
         kitty.enable = lib.mkDefault false; # Disable if not used
-        ghostty.enable = lib.mkDefault true;
-
-        # Shell
-        zsh.enable = true;
-        bash.enable = true;
-
-        # Terminal multiplexer
-        # zellij.enable = lib.mkDefault true; # Enable if using zellij
-
-        # Text editors
-        vim.enable = true;
-        neovim.enable = lib.mkDefault false; # Enable if using neovim
-
-        # File managers
-        # lf.enable = lib.mkDefault true; # Enable if using lf
-
-        # Browsers
-        firefox.enable = lib.mkDefault true;
-
-        # Communication
-        # discord.enable = lib.mkDefault true; # Enable if using Discord
-
-        # System tools
-        btop.enable = lib.mkDefault true;
 
         # Rofi
         rofi.enable = true;
