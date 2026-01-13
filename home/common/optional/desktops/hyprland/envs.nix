cb2ea6c1351a6dcdf73b7e41844a3459301d6090
asebrech <alois.sebrechts@gmail.com>
2026-01-13 13:40:23 +0100

    duplicated envs

--- home/common/optional/desktops/hyprland/envs.nix
+++ home/common/optional/desktops/hyprland/envs.nix
@@ -9,11 +9,14 @@
       # Force all apps to use Wayland
       "GDK_BACKEND,wayland,x11,*"
       "QT_QPA_PLATFORM,wayland;xcb"
+      "CLUTTER_BACKEND,wayland"
       "SDL_VIDEODRIVER,wayland"
       "MOZ_ENABLE_WAYLAND,1"
+      "NIXOS_OZONE_WL,1"
       "ELECTRON_OZONE_PLATFORM_HINT,wayland"
       "OZONE_PLATFORM,wayland"
       "XDG_SESSION_TYPE,wayland"
+      "WLR_NO_HARDWARE_CURSORS,1"
 
       # Allow better support for screen sharing (Google Meet, Discord, etc)
       "XDG_CURRENT_DESKTOP,Hyprland"
