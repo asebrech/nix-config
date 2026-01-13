cb27ba4c997f7e1107d82e35688ffcf8b3cf05f1
asebrech <alois.sebrechts@gmail.com>
2026-01-13 11:41:49 +0100

    fix stylix

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -13,14 +13,6 @@
         ignore_empty_input = true;
       };
 
-      background = [
-        {
-          monitor = "";
-          color = "rgb(0, 0, 0)";
-          blur_passes = 3;
-        }
-      ];
-
       animations = {
         enabled = false;
       };
@@ -32,13 +24,9 @@
           position = "0, 0";
           halign = "center";
           valign = "center";
-          inner_color = "rgb(40, 40, 40)";
-          outer_color = "rgb(100, 100, 100)";
           outline_thickness = 4;
           font_family = "JetBrainsMono Nerd Font";
-          font_color = "rgb(255, 255, 255)";
           placeholder_text = "Enter Password";
-          check_color = "rgb(100, 200, 100)";
           fail_text = "<i>$FAIL ($ATTEMPTS)</i>";
           rounding = 0;
           shadow_passes = 0;
