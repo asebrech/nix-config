44532f873c493f23938ce470e2e6e1310bf8648a
asebrech <alois.sebrechts@gmail.com>
2026-01-13 11:42:51 +0100

    fix stylix

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -17,23 +17,6 @@
         enabled = false;
       };
 
-      input-field = [
-        {
-          monitor = "";
-          size = "650, 100";
-          position = "0, 0";
-          halign = "center";
-          valign = "center";
-          outline_thickness = 4;
-          font_family = "JetBrainsMono Nerd Font";
-          placeholder_text = "Enter Password";
-          fail_text = "<i>$FAIL ($ATTEMPTS)</i>";
-          rounding = 0;
-          shadow_passes = 0;
-          fade_on_empty = false;
-        }
-      ];
-
       auth = {
         "fingerprint:enabled" = true;
       };
