b8333f7bc0afa4b65a92e3d6ed7a89ff21b3ffd0
asebrech <alois.sebrechts@gmail.com>
2026-01-17 10:06:24 +0100

    hyprlock

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -1,6 +1,6 @@
-{ ... }:
+{ config, ... }:
 {
-  # Let Stylix handle all theming (colors, wallpaper, fonts, input-field)
+  # Let Stylix handle colors and wallpaper
   stylix.targets.hyprlock = {
     enable = true;
     useWallpaper = true;
@@ -18,6 +18,125 @@
       general = {
         ignore_empty_input = true;
       };
+
+      # Background with Stylix wallpaper (blur from Style-10)
+      background = [
+        {
+          monitor = "";
+          blur_passes = 2;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
+        }
+      ];
+
+      # Day label
+      label = [
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo -e "$(date +"%A")"'';
+          font_size = 90;
+          font_family = config.stylix.fonts.sansSerif.name;
+          position = "0, 350";
+          halign = "center";
+          valign = "center";
+        }
+        # Date-Month label
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo -e "$(date +"%d %B")"'';
+          font_size = 40;
+          font_family = config.stylix.fonts.sansSerif.name;
+          position = "0, 250";
+          halign = "center";
+          valign = "center";
+        }
+        # Time label
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo "<span>$(date +"- %I:%M -")</span>"'';
+          font_size = 20;
+          font_family = config.stylix.fonts.sansSerif.name;
+          position = "0, 190";
+          halign = "center";
+          valign = "center";
+        }
+        # User label
+        {
+          monitor = "";
+          text = "    $USER";
+          font_size = 18;
+          font_family = config.stylix.fonts.sansSerif.name;
+          position = "0, -130";
+          halign = "center";
+          valign = "center";
+        }
+        # Reboot button
+        {
+          monitor = "";
+          text = "󰜉";
+          font_size = 50;
+          onclick = "reboot now";
+          position = "0, 100";
+          halign = "center";
+          valign = "bottom";
+        }
+        # Power off button
+        {
+          monitor = "";
+          text = "󰐥";
+          font_size = 50;
+          onclick = "shutdown now";
+          position = "820, 100";
+          halign = "left";
+          valign = "bottom";
+        }
+        # Suspend button
+        {
+          monitor = "";
+          text = "󰤄";
+          font_size = 50;
+          onclick = "systemctl suspend";
+          position = "-820, 100";
+          halign = "right";
+          valign = "bottom";
+        }
+      ];
+
+      # User box shape
+      shape = [
+        {
+          monitor = "";
+          size = "300, 60";
+          rounding = -1;
+          border_size = 0;
+          rotate = 0;
+          xray = false;
+          position = "0, -130";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      # Input field (Stylix handles colors)
+      input-field = [
+        {
+          monitor = "";
+          size = "300, 60";
+          outline_thickness = 2;
+          dots_size = 0.2;
+          dots_spacing = 0.2;
+          dots_center = true;
+          fade_on_empty = false;
+          font_family = config.stylix.fonts.sansSerif.name;
+          placeholder_text = ''<i><span foreground="##ffffff99">🔒 Enter Pass</span></i>'';
+          hide_input = false;
+          position = "0, -210";
+          halign = "center";
+          valign = "center";
+        }
+      ];
 
       animations = {
         enabled = false;
