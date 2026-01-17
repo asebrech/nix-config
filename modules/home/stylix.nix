493ecb33778d1e1f28a727edde5f82a4f8c3b546
asebrech <alois.sebrechts@gmail.com>
2026-01-17 07:41:38 +0100

    vicinae stylix

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -5,6 +5,11 @@
 }:
 {
   config = lib.mkIf osConfig.hostSpec.isAutoStyled {
+    # Disable Stylix targets for apps we don't use
+    stylix.targets = {
+      anki.enable = false;
+    };
+
     # Stylix targets are automatically enabled via autoEnable in the host module
     # Add explicit target configuration here only if you need to disable or customize specific apps
   };
