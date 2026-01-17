af397d29d83270f6191308f62bb331486a86c595
asebrech <alois.sebrechts@gmail.com>
2026-01-17 09:36:47 +0100

    plymouth basic

--- hosts/common/optional/plymouth.nix
+++ hosts/common/optional/plymouth.nix
@@ -1,34 +1,20 @@
 {
-  lib,
   ...
 }:
 {
   boot = {
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "udev.log_priority=3"
-    ];
+    plymouth.enable = true;
 
-    plymouth = {
-      enable = true;
-      # Let Stylix handle theming automatically
-    };
-
-    # Enable "Silent Boot"
+    # Standard silent boot configuration
     consoleLogLevel = 3;
     initrd.verbose = false;
 
-    # Enable systemd in initrd for better plymouth integration
-    # Required for Plymouth to work properly
+    kernelParams = [
+      "quiet"
+      "splash"
+    ];
+
+    # Enable systemd in initrd (recommended for Plymouth)
     initrd.systemd.enable = true;
-
-    # Hide bootloader menu (press any key during boot to show it)
-    loader.timeout = lib.mkDefault 0;
   };
-
-  # Increase configuration limit when using plymouth
-  # (initrd can expand significantly with themes)
-  boot.loader.systemd-boot.configurationLimit = lib.mkDefault 10;
 }
