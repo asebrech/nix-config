52fd9f1fb19fc30da38e9e388116ec51bab13c2d
asebrech <alois.sebrechts@gmail.com>
2026-01-17 09:15:21 +0100

    playmouth

--- hosts/common/optional/plymouth.nix
+++ hosts/common/optional/plymouth.nix
@@ -1,29 +1,24 @@
 {
   lib,
-  pkgs,
   ...
 }:
 {
   boot = {
     kernelParams = [
-      "quiet" # Reduce kernel output during boot
-      "splash" # Enable splash screen
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
     ];
 
     plymouth = {
       enable = true;
-      # Note: Stylix will override this theme with its own styling if it has plymouth support
-      # Otherwise we fall back to this default theme
-      theme = lib.mkDefault "hexagon_hud";
-      themePackages = lib.mkDefault [
-        (pkgs.adi1090x-plymouth-themes.override {
-          selected_themes = [ "hexagon_hud" ];
-        })
-      ];
+      # Let Stylix handle theming automatically
     };
 
-    # Reduce console log level for cleaner boot
-    consoleLogLevel = 0;
+    # Enable "Silent Boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
 
     # Enable systemd in initrd for better plymouth integration
     initrd.systemd.enable = lib.mkDefault true;
