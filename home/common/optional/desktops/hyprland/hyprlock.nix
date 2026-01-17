2a99379693e92fa50f71624962ffbd43bcfe7c1e
asebrech <alois.sebrechts@gmail.com>
2026-01-17 10:29:36 +0100

    hyprlock

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -7,34 +7,25 @@
         ignore_empty_input = true;
       };
 
-      # Day label
+      # Style-3 labels
       label = [
+        # Day-Month-Date
         {
           monitor = "";
-          text = ''cmd[update:1000] echo -e "$(date +"%A")"'';
-          font_size = 90;
+          text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
+          font_size = 25;
           font_family = config.stylix.fonts.sansSerif.name;
           position = "0, 350";
           halign = "center";
           valign = "center";
         }
-        # Date-Month label
+        # Time
         {
           monitor = "";
-          text = ''cmd[update:1000] echo -e "$(date +"%d %B")"'';
-          font_size = 40;
+          text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
+          font_size = 120;
           font_family = config.stylix.fonts.sansSerif.name;
           position = "0, 250";
-          halign = "center";
-          valign = "center";
-        }
-        # Time label
-        {
-          monitor = "";
-          text = ''cmd[update:1000] echo "<span>$(date +"- %I:%M -")</span>"'';
-          font_size = 20;
-          font_family = config.stylix.fonts.sansSerif.name;
-          position = "0, 190";
           halign = "center";
           valign = "center";
         }
@@ -47,36 +38,6 @@
           position = "0, -130";
           halign = "center";
           valign = "center";
-        }
-        # Reboot button
-        {
-          monitor = "";
-          text = "󰜉";
-          font_size = 50;
-          onclick = "reboot now";
-          position = "0, 100";
-          halign = "center";
-          valign = "bottom";
-        }
-        # Power off button
-        {
-          monitor = "";
-          text = "󰐥";
-          font_size = 50;
-          onclick = "shutdown now";
-          position = "820, 100";
-          halign = "left";
-          valign = "bottom";
-        }
-        # Suspend button
-        {
-          monitor = "";
-          text = "󰤄";
-          font_size = 50;
-          onclick = "systemctl suspend";
-          position = "-820, 100";
-          halign = "right";
-          valign = "bottom";
         }
       ];
 
@@ -95,7 +56,7 @@
         }
       ];
 
-      # Input field (Style-10 layout with Stylix colors)
+      # Input field (Style-3 layout with Stylix colors)
       input-field = lib.mkForce [
         {
           monitor = "";
@@ -104,8 +65,8 @@
           dots_size = 0.2;
           dots_spacing = 0.2;
           dots_center = true;
-          # Use Stylix colors instead of hardcoded grays
-          outer_color = "rgba(${config.lib.stylix.colors.base05-rgb-r}, ${config.lib.stylix.colors.base05-rgb-g}, ${config.lib.stylix.colors.base05-rgb-b}, 0)";
+          # Use Stylix colors
+          outer_color = "rgba(0, 0, 0, 0)";
           inner_color = "rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, 0.5)";
           font_color = "rgb(${config.lib.stylix.colors.base05-rgb-r}, ${config.lib.stylix.colors.base05-rgb-g}, ${config.lib.stylix.colors.base05-rgb-b})";
           fade_on_empty = false;
