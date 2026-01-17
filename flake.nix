caec091e14dcc261effb8eab0c8983a132966270
asebrech <alois.sebrechts@gmail.com>
2026-01-17 08:31:41 +0100

    update to stylix

--- flake.nix
+++ flake.nix
@@ -156,7 +156,7 @@
     # ========= Theming =========
     #
     stylix = {
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
