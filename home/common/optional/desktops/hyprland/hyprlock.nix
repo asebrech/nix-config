00ee02f02fecc4847be81bb72e6db512bc64ab81
asebrech <alois.sebrechts@gmail.com>
2026-01-18 09:56:03 +0100

    hyprlock

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -1,14 +1,12 @@
 # Hyprlock - Lock screen configuration
 # Adapted from ML4W dotfiles: https://github.com/mylinuxforwork/dotfiles
 # Colors handled by Stylix, layout and structure from ML4W
-{ config, ... }:
 {
-  # Stylix handles colors and base styling
-  stylix.targets.hyprlock = {
-    enable = true;
-    useWallpaper = true;
-  };
-
+  config,
+  lib,
+  ...
+}:
+{
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -18,8 +16,8 @@
 
       # Note: background is configured by Stylix
 
-      # Input field - ML4W layout with Stylix colors overridden
-      input-field = [
+      # Input field - ML4W layout with mkForce to override Stylix
+      input-field = lib.mkForce [
         {
           monitor = "";
           size = "200, 50";
@@ -35,6 +33,11 @@
           hide_input = false;
           rounding = 10;
           fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+          capslock_color = -1;
+          numlock_color = -1;
+          bothlock_color = -1;
+          invert_numlock = false;
+          swap_font_color = false;
           position = "0, -20";
           halign = "center";
           valign = "center";
