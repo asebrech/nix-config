7ffb989b99aed33404c2fa55d2e4e8eab297a374
asebrech <alois.sebrechts@gmail.com>
2026-01-18 10:08:55 +0100

    hyprlock

--- home/common/optional/desktops/hyprland/hyprlock.nix
+++ home/common/optional/desktops/hyprland/hyprlock.nix
@@ -3,7 +3,6 @@
 # Colors handled by Stylix, layout and structure from ML4W
 {
   config,
-  lib,
   ...
 }:
 {
@@ -14,38 +13,7 @@
         ignore_empty_input = true;
       };
 
-      # Note: background is configured by Stylix
-
-      # Input field - ML4W layout with mkForce to override Stylix
-      input-field = lib.mkForce [
-        {
-          monitor = "";
-          size = "200, 50";
-          dots_size = 0.33;
-          dots_spacing = 0.15;
-          dots_center = true;
-          dots_rounding = -1;
-          font_family = config.stylix.fonts.sansSerif.name;
-          outline_thickness = 3;
-          fade_on_empty = true;
-          fade_timeout = 1000;
-          placeholder_text = "<i>Input Password...</i>";
-          hide_input = false;
-          rounding = 10;
-          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-          capslock_color = -1;
-          numlock_color = -1;
-          bothlock_color = -1;
-          invert_numlock = false;
-          swap_font_color = false;
-          position = "0, -20";
-          halign = "center";
-          valign = "center";
-          shadow_passes = 10;
-          shadow_size = 20;
-          shadow_boost = 1.6;
-        }
-      ];
+      # Note: background and input-field are configured by Stylix
 
       # Clock label (top right)
       label = [
