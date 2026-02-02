0853b84d03c5bb799aa21441d877b46472e5422f
asebrech <>
2026-02-02 10:01:08 +0100

    hyprdynamic dynamic

--- home/common/optional/desktops/hyprdynamicmonitors.nix
+++ home/common/optional/desktops/hyprdynamicmonitors.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  laptopMonitor = builtins.head osConfig.monitors;
+  primaryMonitor = builtins.head osConfig.monitors;
 in
 {
   home.hyprdynamicmonitors = {
@@ -22,22 +22,20 @@ in
       disabled = false
       timeout_ms = 3000
 
-      [profiles.laptop_only]
-      config_file = "${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/laptop-only.conf"
+      [profiles.single]
+      config_file = "${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/single.conf"
       config_file_type = "static"
 
-      [[profiles.laptop_only.conditions.required_monitors]]
-      name = "${laptopMonitor.name}"
+      [[profiles.single.conditions.required_monitors]]
+      name = "${primaryMonitor.name}"
 
-      [profiles.mirrored]
-      config_file = "${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/mirrored.conf"
-      config_file_type = "static"
+      [profiles.with_external]
+      config_file = "${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/external.go.tmpl"
+      config_file_type = "template"
 
-      [[profiles.mirrored.conditions.required_monitors]]
-      name = "${laptopMonitor.name}"
-
-      [[profiles.mirrored.conditions.required_monitors]]
-      name = "HDMI-A-1"
+      [[profiles.with_external.conditions.required_monitors]]
+      name = "${primaryMonitor.name}"
+      monitor_tag = "laptop"
     '';
 
     extraFlags = [
@@ -49,14 +47,16 @@ in
     installThemes = false;
   };
 
-  home.file."${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/laptop-only.conf".text =
-    ''
-      monitor = ${laptopMonitor.name},${toString laptopMonitor.width}x${toString laptopMonitor.height}@${toString laptopMonitor.refreshRate},${toString laptopMonitor.x}x${toString laptopMonitor.y},${toString laptopMonitor.scale}
-    '';
+  home.file."${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/single.conf".text = ''
+    monitor = ${primaryMonitor.name},${toString primaryMonitor.width}x${toString primaryMonitor.height}@${toString primaryMonitor.refreshRate},${toString primaryMonitor.x}x${toString primaryMonitor.y},${toString primaryMonitor.scale}
+  '';
 
-  home.file."${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/mirrored.conf".text =
+  home.file."${config.home.homeDirectory}/.config/hyprdynamicmonitors/profiles/external.go.tmpl".text =
     ''
-      monitor = HDMI-A-1,3840x2160@60,0x0,1.0
-      monitor = ${laptopMonitor.name},${toString laptopMonitor.width}x${toString laptopMonitor.height}@${toString laptopMonitor.refreshRate},0x0,1.0,mirror,HDMI-A-1
+      {{- $laptop := index .MonitorsByTag "laptop" -}}
+      {{- range .ExtraMonitors}}
+      monitor={{.Name}},preferred,0x0,1.0
+      monitor={{$laptop.Name}},${toString primaryMonitor.width}x${toString primaryMonitor.height}@${toString primaryMonitor.refreshRate},0x0,1.0,mirror,{{.Name}}
+      {{- end }}
     '';
 }
